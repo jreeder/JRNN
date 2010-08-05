a544e02ce2a87a55d38bf19d8fc52b9fe0b704a4
John Reeder <jreeder@gmail.com>
2010-08-05 16:40:44 -0700

    Added Threading to the test.
    Need to compile the boost threading libraries before I can use it though

--- src/JRNN_test2/JRNN_test2.cpp
+++ src/JRNN_test2/JRNN_test2.cpp
@@ -6,11 +6,18 @@
 #include "trainers/RPropTrainer.h"
 #include "trainers/CCTrainer.h"
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <ctime>
+#include <boost/thread.hpp>
 
 using namespace JRNN;
 using namespace std;
+
+void printDoubles(doubles toPrint, iostream& stream);
+void CCWorker(CCTrainer& trainer, strings& results, int numRuns, bool useValidation = true);
+void BPWorker(RPropTrainer& trainer, int numHid, strings& results, int numRuns, bool useValidation = true);
+
 
 int main(int argc, char* argv[])
 {
@@ -34,12 +41,12 @@ int main(int argc, char* argv[])
 	mds->DistData(50, 100, 200);
 	mds->ImpoverishPrimaryTaskTraining(0.7);
 	ints mtlPrimaryIndexes = mds->GetIndexes("task-1");
-	DatasetPtr ds = mds->SpawnDS();
+	DatasetPtr mtl_ds = mds->SpawnDS();
 	view.clear();
 	view.push_back("task-1");
 	mds->SetView(view);
 	mds->DistData(10, 100, 200);
-	DatasetPtr ds2 = mds->SpawnDS();
+	DatasetPtr stl_ds = mds->SpawnDS();
 	delete mds;
 	mds = NULL;
 
@@ -52,10 +59,113 @@ int main(int argc, char* argv[])
 	CCNetworkPtr ccnet_mtl = CCNetwork::Create();
 	ccnet_stl->Build(2,1);
 	ccnet_mtl->Build(2,4);
-	RPropTrainer bp_stl(ffnet_stl,ds2,1.2,0.5);
-	RPropTrainer bp_mtl(ffnet_mtl,ds, 1.2, 0.5, mtlPrimaryIndexes);
-	CCTrainer cc_stl(ccnet_stl, ds2, 8);
-	CCTrainer cc_mtl(ccnet_mtl, ds, 8, mtlPrimaryIndexes);
+	RPropTrainer bp_stl(ffnet_stl,stl_ds,1.2,0.5);
+	RPropTrainer bp_mtl(ffnet_mtl,mtl_ds, 1.2, 0.5, mtlPrimaryIndexes);
+	CCTrainer cc_stl(ccnet_stl, stl_ds, 8);
+	CCTrainer cc_mtl(ccnet_mtl, mtl_ds, 8, mtlPrimaryIndexes);
+	
+	strings bpstlresults;
+	strings bpmtlresults;
+	strings ccstlresults;
+	strings ccmtlresults;
+
+	boost::thread bpstlthread(BPWorker, bp_stl, 4, bpstlresults, 2, true);
+	boost::thread bpmtlthread(BPWorker, bp_mtl, 16, bpmtlresults, 2, true);
+	boost::thread ccstlthread(CCWorker, cc_stl, ccstlresults, 2, true);
+	boost::thread ccmtlthread(CCWorker, cc_mtl, ccmtlresults, 2, true);
+
+	bpstlthread.join();
+	bpmtlthread.join();
+	ccstlthread.join();
+	ccmtlthread.join();
+
 	return 0;
 }
 
+void BPWorker(RPropTrainer& trainer, int numHid, strings& results, int numRuns, bool useValidation){
+	stringstream output(stringstream::out);
+	for (int i = 0; i < numRuns; i++){
+		double time = 0.0;
+		clock_t startTime = clock();
+		if (useValidation){
+			trainer.TrainToValConv(3000);
+		}
+		else {
+			trainer.TrainToConvergence(0.04, 3000);
+		}
+		time = (clock() - startTime)/(double)CLOCKS_PER_SEC;
+		int epochs = trainer.GetEpochs();
+		output << epochs << "\t";
+		output << time << "\t";
+		//cout << epochs << "\t";
+		output << numHid << "\t";
+		//cout << numHid << "\t";
+		hashedDoubleMap testresults = trainer.TestWiClass(Dataset::TEST);
+		std::pair<string,double> p;
+		BOOST_FOREACH(p, testresults){
+			output << p.first << ":" << p.second << "\t";
+			//cout << p.first << ":" << p.second << "\t";
+		}
+		output << "|\t";
+		printDoubles(trainer.GetMSERec(),output);
+		output << endl;
+		//cout << endl;
+		results.push_back(output.str());
+		output.str("");
+		output.clear();
+		trainer.Reset();
+	}
+}
+
+void CCWorker(CCTrainer& trainer, strings& results, int numRuns, bool useValidation){
+	stringstream output(stringstream::out);
+	for (int i = 0; i < numRuns; i++)
+	{
+		double time = 0.0;
+		clock_t startTime = clock();
+		if(useValidation){
+			trainer.TrainToValConv(3000);
+		}
+		else {
+			trainer.TrainToConvergence(3000);
+		}
+		time = (clock() - startTime)/(double)CLOCKS_PER_SEC;
+
+		int epochs = trainer.GetEpochs();
+		int hiddenLayers = trainer.GetNumHidLayers();
+		output << epochs << "\t";
+		output << time << "\t";
+		output << hiddenLayers << "\t";
+#ifdef _DEBUG
+		cout << epochs << "\t";
+		cout << hiddenLayers << "\t";
+		cout << time << "\t";
+#endif // _DEBUG
+
+
+		hashedDoubleMap testresults = trainer.TestWiClass(Dataset::TEST);
+		std::pair<string,double> p;
+		BOOST_FOREACH(p, testresults){
+			output << p.first << ":" << p.second << "\t";
+#ifdef _DEBUG
+			cout << p.first << ":" << p.second << "\t";
+#endif // _DEBUG
+		}
+		output << "|\t";
+		printDoubles(trainer.GetMSERec(),output);
+		output << endl;
+#ifdef _DEBUG
+		cout << endl;
+#endif // _DEBUG
+		results.push_back(output.str());
+		output.str("");
+		output.clear();
+		trainer.Reset();
+	}
+}
+
+void printDoubles(doubles toPrint, iostream& stream){
+	BOOST_FOREACH(double db, toPrint){
+		stream << db << "\t";
+	}
+}
