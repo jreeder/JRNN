95a109734a4285209d5fdf1d180d006e95ec77b3
unknown <John Reeder@jreeder-mbp-win7.(none)>
2011-11-19 19:22:04 -0500

    updating config

--- Experiments/JRNN_Experiments/src/config.py
+++ Experiments/JRNN_Experiments/src/config.py
@@ -18,8 +18,8 @@ datapath = basepath + r"/Experiments/Data/Tabbed Data/New Binary Tasks/"
 datapath2 = basepath + r"/Experiments/Data/Tabbed Data/UCI Data/"
 outpath = basepath + r"/Experiments/Results/Exp 1-1/Round-4/"
 outpath2 = basepath + r"/Experiments/Results/Exp 2-1/Round 5/"
-outpath3 = basepath + r"/Experiments/Results/Exp 2-1/Round 11/" #8 is best run 9 is new csmtl stuff 10 is all ds with csmtl ccmtl and etamtl and stltests
-#Round 11 has the new experiments with more impoverished sets. 
+outpath3 = basepath + r"/Experiments/Results/Exp 2-1/Round 12/" #8 is best run 9 is new csmtl stuff 10 is all ds with csmtl ccmtl and etamtl and stltests
+#Round 11 has the new experiments with more impoverished sets 12 has some reruns. 
 genericout = basepath + r"/Experiments/Results/"
 jrnn_exe2 = basepath + r"/Builds/VS2010/JRNN/Release/JRNN_test2.exe"
 jrnn_exprun = basepath + r"/Builds/VS2010/JRNN/Release/JRNN_exprun.exe"
