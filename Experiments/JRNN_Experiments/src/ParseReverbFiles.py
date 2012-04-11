edb76a2acc720064e3e7f2a717fcb85493b45339
jreeder <jreeder@gmail.com>
2012-04-11 00:55:00 -0400

    Scripts to parse and plot the reverb experiment files.

---
+++ Experiments/JRNN_Experiments/src/ParseReverbFiles.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  jreeder --<jreeder@gmail.com>
+# Purpose: This is meant to parse the cc csmtl reverberation files. 
+# Created: 4/10/2012
+
+import os
+
+class Bunch(object):
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+def ParseFile(filename):
+    filepath, filen = os.path.split(filename)
+    thisName, ext = os.path.splitext(filen)  
+    
+    infile = open(filename, 'r')
+    epochsArr = []
+    timesArr = []
+    errorsArr = []
+    hidLayersArr = []
+    epochMSEsArr = []
+    resetsArr = []
+    firstDSErrArr = []
+    firstDSErrsWhileTrain = []
+    
+    for line in infile:
+        firstsplit = line.split("|")
+        if len(firstsplit) < 4: continue
+        #results of the second task training
+        secondTaskMetrics = firstsplit[0].split("\t")
+        secondTaskrunMSEs = [float(x) for x in firstsplit[1].split("\t")[1:-1]]
+        epochsArr.append(int(secondTaskMetrics[0]))
+        timesArr.append(float(secondTaskMetrics[1]))
+        hidLayersArr.append(int(secondTaskMetrics[2]))
+        resetsArr.append(int(secondTaskMetrics[3]))
+        tmpDict = {}
+        for item in secondTaskMetrics[4:]:
+            itemsplit = item.split(":")
+            if len(itemsplit) > 1:
+                tmpDict[itemsplit[0]] = float(itemsplit[1])
+        errorsArr.append(tmpDict)
+        epochMSEsArr.append(secondTaskrunMSEs)
+        
+        #results from the first task trained while the second is training
+        firsttaskerrs = firstsplit[2].split("\t")
+        tmpDict.clear()
+        for item in firsttaskerrs:
+            itemsplit = item.split(":")
+            if len(itemsplit) > 1:
+                tmpDict[itemsplit[0]] = float(itemsplit[1])
+        firstDSErrArr.append(tmpDict)
+        testWhileTrain = [x.strip() for x in firstsplit[3].split("*")]
+        tmpDict.clear()
+        for item in testWhileTrain:
+            itemsplit = item.split("!")
+            if len(itemsplit) > 1:
+                secsplit = itemsplit[1].split(":")
+                tmpDict[int(itemsplit[0])] = secsplit[1]
+        firstDSErrsWhileTrain.append(tmpDict)
+    
+    infile.close()
+    retVal = Bunch(name = thisName, resets=resetsArr,\
+        epochs=epochsArr, times=timesArr, errors=errorsArr, hidLayers=hidLayersArr,\
+        epochMSEs = epochMSEsArr, ferrors = firstDSErrArr, ftwtrain = firstDSErrsWhileTrain)
+    
+    return retVal
---
+++ Experiments/JRNN_Experiments/src/PlotReverbFiles.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  jreeder --<jreeder@gmail.com>
+# Purpose: Quick Plot of the reverberation stuff. 
+# Created: 4/10/2012
+
+import sys
+import unittest
+
+import os
+import ParseReverbFiles as pe
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from config import *
+
+
+
+def MakeGraph(title, filepath):
+    title = title + "First Task Performance"
+    data = pe.ParseFile(filepath)
+    ftperf = data.ftwtrain
+    minlen = float("inf")
+    for d in ftperf:
+        nlen = len(d)
+        if nlen < minlen: minlen = nlen
+    tmpArr = [[] for i in range(minlen)]
+    
+    for d in ftperf:
+        sorteddict = [x for x in d.iteritems()]
+        sorteddict.sort(key=lambda x: x[0])
+        count = 0
+        for x in sorteddict:
+            if count == minlen: break
+            tmpArr[count].append(float(x[1]))
+            count += 1
+        
+    tmpArr2 = [float(sum(x))/len(x) for x in tmpArr]
+    
+    pArray = np.array(tmpArr2)
+    plot(pArray)
