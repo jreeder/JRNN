baf50e83ec0ed2a88c7898cb7490342949ee2b30
jreeder <jreeder@gmail.com>
2012-08-17 11:36:00 -0400

    Added the hashedDoubleMap python conversion
    Added hashedDoubleMap python conversion since it is a return type from the
    trainers.

--- include/PyJRNN/JRNNTypeDefs.h
+++ include/PyJRNN/JRNNTypeDefs.h
@@ -14,6 +14,7 @@
 
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/python/suite/indexing/map_indexing_suite.hpp>
 #define JRNN_DO_IMPORT
 #include <JRNN.h>
 
@@ -49,6 +50,53 @@ struct std_item {
 	}
 };
 
+//template<class T>
+//struct map_item
+//{
+//	typedef typename T::key_type K;
+//	typedef typename T::mapped_type V;
+//	static V& get(T const& x, K const& i)
+//	{
+//		if( x.find(i) != x.end() ) return x[i];
+//		KeyError();
+//	}
+//	static void set(T const& x, K const& i, V const& v)
+//	{
+//		x[i]=v; // use map autocreation feature
+//	}
+//	static void del(T const& x, K const& i)
+//	{
+//		if( x.find(i) != x.end() ) x.erase(i);
+//		else KeyError();
+//	}
+//	static bool in(T const& x, K const& i)
+//	{
+//		return x.find(i) != x.end();
+//	}
+//	static list keys(T const& x)
+//	{
+//		list t;
+//		for(T::const_iterator it=x.begin; it!=x.end(); ++it)
+//			t.append(it->first);
+//		return t;
+//	}
+//	static list values(T const& x)
+//	{
+//		list t;
+//		for(T::const_iterator it=x.begin; it!=x.end(); ++it)
+//			t.append(it->second);
+//		return t;
+//	}
+//	static list items(T const& x)
+//	{
+//		list t;
+//		for(T::const_iterator it=x.begin; it!=x.end(); ++it)
+//			t.append(make_tuple(it->first,it->second));
+//		return t;
+//	}
+//};
+//void KeyError() { PyErr_SetString(PyExc_KeyError, "Key not found"); }
+
 void exportTypeDefs(){
 
 	class_<doubles>("doubles").
@@ -70,8 +118,12 @@ void exportTypeDefs(){
 			return_value_policy<copy_non_const_reference>()).
 		def("__setitem__", &std_item<matDouble>::set,
 			with_custodian_and_ward<1,2>()). // To let container keep value
-		def("__delitem__", &std_item<matDouble>::del)
+		def("__delitem__", &std_item<matDouble>::del).
+		def("__iter__", iterator<matDouble>())
 		;
+
+	class_<hashedDoubleMap>("hashedDoubleMap").
+		def(map_indexing_suite<hashedDoubleMap>());
 }
 
 #endif
